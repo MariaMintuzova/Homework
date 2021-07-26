ba3a947accded7f6238ccd48c934c54c9353e375
MariyaMintuzova <mmintuzova@gmail.com>
2021-07-27 01:08:23 +0300

    Homework lesson5

---
+++ src/main/java/ru/geekbrains/lesson5/Homework5main.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson5;
+
+public class Homework5main {
+    public static void main(String[] args) {
+
+    Worker person1 = new Worker("Pablo", "operator", "123@gm.com",
+            "+79991232323", 1200,29);
+
+    person1.information();
+    }
+
+}
